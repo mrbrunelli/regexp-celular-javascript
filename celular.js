cc988badefe1cbce5230ad77e72217ef3789104c
gcairesdev <pessoalgcaires@gmail.com>
2020-05-13 18:51:31 -0300

    Refatora a escrita de variáveis, para melhor vizualização

--- celular.js
+++ celular.js
@@ -1,16 +1,17 @@
 // Função que retorna valor em tempo real, dando efeito de mask
 function formatarCelular(event) {
     var input = event.target;
-    var celular = event.target.value
+    var numero = event.target.value;
+    var tamanho = numero.length;   
 
     // Remover todo caractere que não seja dígito
-    celular = celular.replace(/(\D)/g, '')
+    numero = numero.replace(/(\D)/g, '');
 
     // Verificações por etapa. Vou concatenando os parênteses e o traço conforme a progressão evolui
-    if (celular.length >= 11) celular = celular.replace(/^(\d{2})(\d{1})(\d{4})(\d{4})$/, '($1) $2 $3-$4')
-    if (celular.length >= 8) celular = celular.replace(/^(\d{2})(\d{1})(\d{4})/, '($1) $2 $3-')
-    if (celular.length >= 4) celular = celular.replace(/^(\d{2})(\d{1})/, '($1) $2 ')
-    if (celular.length >= 3) celular = celular.replace(/^(\d{2})/, '($1) ')
+    if (tamanho >= 11) numero = numero.replace(/^(\d{2})(\d{1})(\d{4})(\d{4})$/, '($1) $2 $3-$4');
+    if (tamanho >= 8) numero = numero.replace(/^(\d{2})(\d{1})(\d{4})/, '($1) $2 $3-');
+    if (tamanho >= 4) numero = numero.replace(/^(\d{2})(\d{1})/, '($1) $2 ');
+    if (tamanho >= 3) numero = numero.replace(/^(\d{2})/, '($1) ');
 
-    input.value = celular;
+    input.value = numero;
 }
